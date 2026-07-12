b6437ac9a0ffd7a20045477b4b08adb2bd9d9540
Huxlyx <mtengler@gmail.com>
2026-07-12 23:58:42 +0200

    Added log message for device connection after actual connection attempt

--- mechrain-server/src/de/mechrain/Server.java
+++ mechrain-server/src/de/mechrain/Server.java
@@ -140,13 +140,13 @@ public class Server {
 					final int deviceId = deviceIdByte & 0xFF;
 					final Device device = getRegistry().getOrAddDevice(deviceId);
 					
-					LOG.debug(() -> "Connected to device " + device);
-					
 					/* if device loses connection and shortly after connects again it like still shows as connected */
 					if (device.isConnected()) {
 						device.disconnect();
 					}
 					device.connect(client, is, os);
+					
+					LOG.debug(() -> "Connected to device " + device);
 					/* 45s ~ 900 ml bei 5V 	 -> 20ml/s */
 					/* 45s ~ 600 ml bei 3.3V -> 13ml/s */
 				} catch (IOException e) {
